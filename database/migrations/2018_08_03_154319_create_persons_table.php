1a061dfc4dfba28d76470c815aa773e0d5458ed7
HDVinnie <unit3d@protonmail.com>
2019-01-25 14:16:33 -0500

    (Add) Migration :new:

---
+++ database/migrations/2018_08_03_154319_create_persons_table.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * NOTICE OF LICENSE
+ *
+ * UNIT3D is open-sourced software licensed under the GNU General Public License v3.0
+ * The details is bundled with this project in the file LICENSE.txt.
+ *
+ * @project    UNIT3D
+ * @license    https://www.gnu.org/licenses/agpl-3.0.en.html/ GNU Affero General Public License v3.0
+ * @author     HDVinnie
+ */
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePersonsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('persons', function (Blueprint $table) {
+            $table->increments('id');
+            $table->date('birthday')->nullable();
+            $table->date('deathday')->nullable();
+            $table->string('name');
+            $table->string('gender')->nullable();
+            $table->text('biography')->nullable();
+            $table->string('place_of_birth')->nullable();
+            $table->string('photo')->nullable();
+            $table->bigInteger('tmdb_id')->unsigned()->unique()->index();
+            $table->boolean('aotm')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('persons');
+    }
+}
