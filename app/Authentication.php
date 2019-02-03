20d07fc3f40d5b7ebb026a69ed5e1a0571f66a4e
HDVinnie <unit3d@protonmail.com>
2019-02-03 14:49:09 -0500

    (Add) Models :new:

---
+++ app/Authentication.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * NOTICE OF LICENSE.
+ *
+ * UNIT3D is open-sourced software licensed under the GNU General Public License v3.0
+ * The details is bundled with this project in the file LICENSE.txt.
+ *
+ * @project    UNIT3D
+ *
+ * @license    https://www.gnu.org/licenses/agpl-3.0.en.html/ GNU Affero General Public License v3.0
+ * @author     HDVinnie
+ */
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Authentication extends Model
+{
+    /**
+     * The Attributes That Should Be Casted To Native Types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'user_id'    => 'integer',
+        'device_id'  => 'integer',
+        'ip_address' => 'string',
+    ];
+
+    /**
+     * Belongs To A User.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class)->withDefault([
+            'username' => 'System',
+            'id'       => '1',
+        ]);
+    }
+
+    /**
+     * Belongs To A Device.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function device()
+    {
+        return $this->belongsTo(Device::class);
+    }
+}
